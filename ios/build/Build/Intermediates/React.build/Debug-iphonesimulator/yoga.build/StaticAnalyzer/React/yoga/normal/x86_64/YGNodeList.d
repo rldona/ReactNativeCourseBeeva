901b183f82a46091d7d3fb07bbdcfe0c2fd7b345
Raúl López <rldona@gmail.com>
2017-08-09 12:48:11 +0200

    Init

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/StaticAnalyzer/React/yoga/normal/x86_64/YGNodeList.d
+++ ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/StaticAnalyzer/React/yoga/normal/x86_64/YGNodeList.d
@@ -1,3 +1,6 @@
 dependencies: \
-  /Users/raul.lopez/htdocs/master/trunk/react-native/react-native-course-beeva/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
-  /Users/raul.lopez/htdocs/master/trunk/react-native/react-native-course-beeva/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h
+  /Users/raul.lopez/htdocs/master/trunk/react-native/ReactNativeCourseBeeva/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
+  /Users/raul.lopez/htdocs/master/trunk/react-native/ReactNativeCourseBeeva/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
+  /Users/raul.lopez/htdocs/master/trunk/react-native/ReactNativeCourseBeeva/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
+  /Users/raul.lopez/htdocs/master/trunk/react-native/ReactNativeCourseBeeva/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
+  /Users/raul.lopez/htdocs/master/trunk/react-native/ReactNativeCourseBeeva/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
